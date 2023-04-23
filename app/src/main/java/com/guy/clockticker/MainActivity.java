55fe352c87f526b597ef1c767a11ecf743137754
guymacin <guyskv@gmail.com>
2023-04-23 20:45:20 +0300

    Initial commit

---
+++ app/src/main/java/com/guy/clockticker/MainActivity.java
@@ -0,0 +1,36 @@
+package com.guy.clockticker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.guy.mct.MCT5;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MCT5.initHelper();
+
+        MCT5.get().cycle(new MCT5.CycleTicker() {
+            @Override
+            public void secondly(int repeatsRemaining) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("pttt", "Hi");
+                    }
+                });
+            }
+
+            @Override
+            public void done() {
+
+            }
+        }, MCT5.CONTINUOUSLY_REPEATS, 1000, "MainActivity");
+    }
+}
